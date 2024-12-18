704327caaae728c48a0279f26b30e33457eec4f6
jidai01 <lopesdacruzjeray@gmail.com>
2024-12-18 21:38:18 +0800

    Git_4

--- order/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import PelangganListView, PelangganCreateView, PelangganUpdateView, PelangganDeleteView
-
-urlpatterns = [
-    path('', PelangganListView.as_view(), name='pelanggan_list'),
-    path('create/', PelangganCreateView.as_view(), name='pelanggan_create'),
-    path('update/<str:pk>/', PelangganUpdateView.as_view(), name='pelanggan_update'),
-    path('delete/<str:pk>/', PelangganDeleteView.as_view(), name='pelanggan_delete'),
-]
--- order/views.py
+++ order/views.py
@@ -1,25 +1,3 @@
-from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from .models import Pelanggan
-from .forms import PelangganForm
+from django.shortcuts import render
 
-class PelangganListView(ListView):
-    model = Pelanggan
-    template_name = 'order/pelanggan_list.html'
-
-class PelangganCreateView(CreateView):
-    model = Pelanggan
-    form_class = PelangganForm
-    template_name = 'order/pelanggan_form.html'
-    success_url = reverse_lazy('pelanggan_list')
-
-class PelangganUpdateView(UpdateView):
-    model = Pelanggan
-    form_class = PelangganForm
-    template_name = 'order/pelanggan_form.html'
-    success_url = reverse_lazy('pelanggan_list')
-
-class PelangganDeleteView(DeleteView):
-    model = Pelanggan
-    template_name = 'order/pelanggan_confirm_delete.html'
-    success_url = reverse_lazy('pelanggan_list')
+# Create your views here.
